da6a9c76847b528a1b20b4e660176e3482e121b6
syedmehedi34 <syedmehedi34@gmail.com>
2025-08-30 07:32:31 +0600

    added clerk provider

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,7 +15,7 @@ const Hero = () => {
       </p>
 
       {/* //* form portion starts  */}
-      <form className="bg-white text-gray-500 rounded-lg px-6 py-4  flex flex-col md:flex-row max-md:items-start gap-4 max-md:mx-auto">
+      <form className="bg-white text-gray-500 rounded-lg px-6 py-4 mt-8  flex flex-col md:flex-row max-md:items-start gap-4 max-md:mx-auto">
         <div>
           <div className="flex items-center gap-2">
             <img src={assets?.calenderIcon} className="h-4" alt="" />
