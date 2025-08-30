15b318054631950355d1dacd7e9cb9d24ebe8b24
syedmehedi34 <syedmehedi34@gmail.com>
2025-08-30 07:05:34 +0600

    home added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "./components/Navbar";
-import { useLocation } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
+import Home from "./pages/Home";
 
 const App = () => {
   const isOwnerPath = useLocation().pathname.includes("/owner");
@@ -9,7 +10,12 @@ const App = () => {
     <div>
       {/*  */}
       {!isOwnerPath && <Navbar />}
-      {/*  */}
+
+      <div className="min-h-[70vh]">
+        <Routes>
+          <Route path="/*" element={<Home />} />
+        </Routes>
+      </div>
     </div>
   );
 };
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,9 +2,8 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <div>
-      hero will be here 
-      {/*  */}
+    <div className="flex flex-col items-center justify-center px-6 md:px-16 lg:px-24 xl:px-32 text-white bg-[url('/src/assets/heroImage.png')] bg-no-repeat bg-cover bg-center h-screen">
+      <p>The Ultimate Hotel Experience</p>
     </div>
   );
 };
