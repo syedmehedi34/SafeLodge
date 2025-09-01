d833c4046ff95123086f02d3c88b55bddd191fb5
syedmehedi34 <syedmehedi34@gmail.com>
2025-09-02 01:23:06 +0600

    the layout and sidebar topbar created

--- src/pages/hotelOwner/Layout.jsx
+++ src/pages/hotelOwner/Layout.jsx
@@ -1,9 +1,18 @@
+import { Outlet } from "react-router-dom";
 import Navbar from "../../components/hotelOwner/Navbar";
+import SideBar from "../../components/hotelOwner/SideBar";
 
 const Layout = () => {
   return (
     <div className="flex flex-col h-screen">
       <Navbar />
+      <div className="flex h-full">
+        <SideBar />
+
+        <div className="flex-1 p-4 pt-10 md:px-10 h-full">
+          <Outlet />
+        </div>
+      </div>
     </div>
   );
 };
