f0bf3907b8a0cf92ba18d05b1c3a6ac1434dfdd4
syedmehedi34 <syedmehedi34@gmail.com>
2025-09-02 01:06:43 +0600

    owner path implemented

---
+++ src/pages/hotelOwner/Layout.jsx
@@ -0,0 +1,11 @@
+import Navbar from "../../components/hotelOwner/Navbar";
+
+const Layout = () => {
+  return (
+    <div className="flex flex-col h-screen">
+      <Navbar />
+    </div>
+  );
+};
+
+export default Layout;
