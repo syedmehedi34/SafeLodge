f7fc327d1dc8be1f70936111a44f46ff9dd8f92c
syedmehedi34 <syedmehedi34@gmail.com>
2025-09-06 16:38:49 +0600

    upload images UI done

--- src/pages/hotelOwner/AddRoom.jsx
+++ src/pages/hotelOwner/AddRoom.jsx
@@ -1,10 +1,63 @@
-import React from "react";
+/* eslint-disable no-unused-vars */
+import { useState } from "react";
+import Title from "../../components/Title";
+import { assets } from "../../assets/assets";
 
 const AddRoom = () => {
+  const [images, setImages] = useState({
+    1: null,
+    2: null,
+    3: null,
+    4: null,
+  });
+  const [inputs, setInputs] = useState({
+    roomType: "",
+    pricePerNight: 0,
+    amenities: {
+      "Free Wifi": false,
+      "Free Breakfast": false,
+      "Room Service": false,
+      "Mountain View": false,
+      "Pool Access": false,
+    },
+  });
+
   return (
-    <div>
-      <h1>AddRoom</h1>
-    </div>
+    <form>
+      <Title
+        align="left"
+        font="outfit"
+        title="Add Room"
+        subTitle="Fill in the details carefully and accurate room details, pricing, and amenities, to enhance the user booking experience."
+      />
+
+      {/* upload images area */}
+      <p className="text-gray-800 mt-10">Images</p>
+      <div className="grid grid-cols-2 sm:flex gap-4 my-2 flex-wrap">
+        {Object.keys(images).map((key) => (
+          <label htmlFor={`roomImage${key}`} key={key}>
+            <img
+              className="max-h-13 cursor-pointer opacity-80"
+              src={
+                images[key]
+                  ? URL.createObjectURL(images[key])
+                  : assets.uploadArea
+              }
+              alt=""
+            />
+            <input
+              onChange={(e) =>
+                setImages({ ...images, [key]: e.target.files[0] })
+              }
+              type="file"
+              accept="image/*"
+              id={`roomImage${key}`}
+              hidden
+            />
+          </label>
+        ))}
+      </div>
+    </form>
   );
 };
 
