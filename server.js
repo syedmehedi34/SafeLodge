061ee0f07d8f0a9a6cee9cf5b0d597560dfa5d57
syedmehedi34 <syedmehedi34@gmail.com>
2025-09-07 00:56:30 +0600

    clerk webhook fuction added

--- server.js
+++ server.js
@@ -3,10 +3,15 @@ import express from "express";
 import "dotenv/config";
 import cors from "cors";
 import connectDB from "./config/db.js";
+import { clerkMiddleware } from "@clerk/express";
 
 connectDB();
 const app = express();
 app.use(cors()); // enable cors-origin here
+
+// middleware
+app.use(express.json());
+app.use(clerkMiddleware());
 
 app.get("/", (req, res) => res.send("api is working"));
 
