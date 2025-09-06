bac81f8b9d096106c1a47d81d4f0dd9ed28b41af
syedmehedi34 <syedmehedi34@gmail.com>
2025-09-06 22:38:33 +0600

    first commit

---
+++ server.js
@@ -0,0 +1,13 @@
+// npm i express dotenv cors mongoose cloudinary multer svix, | nodemon
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+
+const app = express();
+app.use(cors()); // enable cors-origin here
+
+app.get("/", (req, res) => res.send("api is working"));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
