f9cf85c0a43facb796a28ec1b66a98392cd49582
syedmehedi34 <syedmehedi34@gmail.com>
2025-09-08 02:08:31 +0600

    toggleRoomAvailability from room controller done

--- server/controllers/roomController.js
+++ server/controllers/roomController.js
@@ -68,4 +68,16 @@ export const getOwnerRooms = async (req, res) => {
 };
 
 // API to toggle availability of a room
-export const toggleRoomAvailability = async (req, res) => {};
+export const toggleRoomAvailability = async (req, res) => {
+  try {
+    const { roomId } = req.body;
+    const roomData = await Room.findById(roomId);
+    roomData.isAvailable = !roomData.isAvailable;
+
+    await roomData.save();
+
+    res.json({ success: true, message: "Room availability updated" });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
