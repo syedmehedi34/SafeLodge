c61391bf96baf9c40d9fb1979de05e37d82f82d2
syedmehedi34 <syedmehedi34@gmail.com>
2025-09-08 12:41:19 +0600

    app context updated, fetchUser function implemented

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,13 +11,14 @@ import Layout from "./pages/hotelOwner/Layout";
 import Dashboard from "./pages/hotelOwner/Dashboard";
 import AddRoom from "./pages/hotelOwner/AddRoom";
 import ListRoom from "./pages/hotelOwner/ListRoom";
+import { Toaster } from "react-hot-toast";
 
 const App = () => {
   const isOwnerPath = useLocation().pathname.includes("/owner");
 
   return (
     <div>
-      {/*  */}
+      <Toaster />
       {!isOwnerPath && <Navbar />}
       {/* <HotelReg /> */}
 
