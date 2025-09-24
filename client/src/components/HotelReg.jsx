e92e4614f8e7ad768b2ef65c2eb7140decbe89eb
syedmehedi34 <syedmehedi34@gmail.com>
2025-09-25 01:06:25 +0600

    hotel registration form works done

--- client/src/components/HotelReg.jsx
+++ client/src/components/HotelReg.jsx
@@ -1,14 +1,51 @@
+import { useState } from "react";
 import { assets, cities } from "../assets/assets";
 import { useAppContext } from "../context/AppContext";
+import toast from "react-hot-toast";
 
 const HotelReg = () => {
-  const { setShowHotelReg } = useAppContext();
+  const { setShowHotelReg, axios, getToken, setIsOwner } = useAppContext();
 
-  // 7:57:00
+  // store form data
+  const [name, setName] = useState("");
+  const [address, setAddress] = useState("");
+  const [contact, setContact] = useState("");
+  const [city, setCity] = useState("");
+
+  const onSubmitHandler = async (event) => {
+    try {
+      event.preventDefault();
+
+      //
+      const { data } = await axios.post(
+        `/api/hotels/`,
+        { name, contact, address, city },
+        {
+          headers: { Authorization: `Bearer ${await getToken()}` },
+        }
+      );
+      if (data.success) {
+        toast.success(data.message);
+        setIsOwner(true);
+        setShowHotelReg(false);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
 
   return (
-    <div className="fixed top-0 bottom-0 left-0 right-0 z-100 flex items-center justify-center bg-black/70">
-      <form className="flex bg-white rounded-xl max-w-4xl max-md:mx-2">
+    <div
+      onClick={() => setShowHotelReg(false)}
+      className="fixed top-0 bottom-0 left-0 right-0 z-100 flex items-center justify-center bg-black/70"
+    >
+      <form
+        onSubmit={onSubmitHandler}
+        onClick={(e) => e.stopPropagation()}
+        className="flex bg-white rounded-xl max-w-4xl max-md:mx-2"
+      >
         <img
           src={assets.regImage}
           alt="reg-image"
@@ -30,6 +67,8 @@ const HotelReg = () => {
               Hotel Name
             </label>
             <input
+              onChange={(e) => setName(e.target.value)}
+              value={name}
               id="name"
               type="text"
               placeholder="Type here"
@@ -44,6 +83,8 @@ const HotelReg = () => {
               Phone
             </label>
             <input
+              onChange={(e) => setContact(e.target.value)}
+              value={contact}
               id="contact"
               type="text"
               placeholder="Type here"
@@ -58,6 +99,8 @@ const HotelReg = () => {
               Address
             </label>
             <input
+              onChange={(e) => setAddress(e.target.value)}
+              value={address}
               id="address"
               type="text"
               placeholder="Type here"
@@ -72,6 +115,8 @@ const HotelReg = () => {
               City
             </label>
             <select
+              onChange={(e) => setCity(e.target.value)}
+              value={city}
               id="city"
               className="border border-gray-200 rounded w-full px-3 py-2.5 mt-1 outline-indigo-500 font-light"
               required
